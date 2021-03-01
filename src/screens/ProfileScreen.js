17a3186348850289511802b4cd3e2b72dc94bbcd
Asad Butt <basad033@gmail.com>
2021-03-01 20:43:17 +0500

    drawer screen changes

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,13 +1,22 @@
 import React,{useEffect} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
+import { useFocusEffect } from '@react-navigation/native';
 
 function ProfileScreen({navigation}){
-    useEffect(()=>{
-        navigation.addListener('focus', () => {
-           // when screen is focused (navigated to)
-           navigation.openDrawer();
-        });  
-      },[navigation])
+    // useEffect(()=>{
+    //     navigation.addListener('focus', () => {
+    //        // when screen is focused (navigated to)
+    //        navigation.openDrawer();
+    //     });  
+    //   },[navigation])
+
+ useFocusEffect(
+    React.useCallback(() => {
+        // navigation.addListener('focus', () => {
+        navigation.openDrawer();
+        // })
+    }, [])
+  );
         return(
         <SafeAreaView>
             {/* <Text>ProfileScreen</Text> */}
--- src/screens/SideMenuScreen.js
+++ src/screens/SideMenuScreen.js
@@ -14,8 +14,9 @@ const WIDTH = Dimensions.get("screen").width;
 
 export default function SideMenuScreen(props)  {
     return (
-        <SafeAreaView style={styles.drawerContainer}> 
-      <View style={styles.upperBG}/>
+        // <SafeAreaView style={styles.drawerContainer}> 
+     <View>
+     <View style={styles.upperBG}/>
 <DrawerItem 
         icon={({color,size})=> (
 <MaterialCommunityIcons  style={{color:"purple"}}
@@ -38,7 +39,8 @@ size={size}/>
 label="Log Out"
    />
      </View>   
-     </SafeAreaView>       
+     </View>
+    //  </SafeAreaView>       
                     )
 }
 
@@ -46,7 +48,7 @@ label="Log Out"
 
 const styles = StyleSheet.create({
     upperBG:{
-        height:100,
+        height:160,
         width:'100%',
         borderBottomLeftRadius:WIDTH,
         borderBottomRightRadius:WIDTH,
